b87251582be5ca0a2445ab79fc5b8c1330a69e07
Jeff Huang <jeffishjeff@gmail.com>
2024-07-18 12:14:53 +0800

    ta2

--- programs/tiny-adventure/src/lib.rs
+++ programs/tiny-adventure/src/lib.rs
@@ -1,4 +1,7 @@
+use min_max::*;
+use anchor_lang::system_program;
 use anchor_lang::prelude::*;
+use anchor_lang::solana_program::native_token::LAMPORTS_PER_SOL;
 
 declare_id!("DXxLHjAjv1xyV367pNLzFGSVdmUSD9UJRGKhjuvxm79D");
 
@@ -6,78 +9,117 @@ declare_id!("DXxLHjAjv1xyV367pNLzFGSVdmUSD9UJRGKhjuvxm79D");
 pub mod tiny_adventure {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        ctx.accounts.new_game_data_account.player_position = 0;
+    const MIN_LOOT: u64 = LAMPORTS_PER_SOL / 10; // 0.1 SOL
+
+    pub fn initialize(_ctx: Context<Initialize>) -> Result<()> {
         msg!("A Journey Begins!");
-        msg!("o.......");
         Ok(())
     }
 
-    pub fn move_left(ctx: Context<MoveLeft>) -> Result<()> {
-        let game_data_account = &mut ctx.accounts.game_data_account;
-        if game_data_account.player_position == 0 {
-            msg!("You are back at the start.");
-        } else {
-            game_data_account.player_position -= 1;
-            print_player(game_data_account.player_position);
-        }
+    pub fn new_game(ctx: Context<NewGame>, loot: u64) -> Result<()> {
+        let player_account = &mut ctx.accounts.player_account;
+        let has_loot = **ctx.accounts.loot_account.to_account_info().try_borrow_mut_lamports()? > 0;
+        
+        let cpi_context = CpiContext::new(
+            ctx.accounts.system_program.to_account_info(),
+            system_program::Transfer {
+                from: ctx.accounts.payer.to_account_info().clone(),
+                to: ctx.accounts.loot_account.to_account_info().clone(),
+            },
+        );
+        system_program::transfer(cpi_context, max!(loot, MIN_LOOT))?;
+
+        player_account.position = 0;
+        msg!("New game started!");
+        print_game(player_account.position, has_loot);
         Ok(())
     }
 
-    pub fn move_right(ctx: Context<MoveRight>) -> Result<()> {
-        let game_data_account = &mut ctx.accounts.game_data_account;
-        if game_data_account.player_position == 3 {
-            msg!("You have reached the end! Super!");
+    pub fn travel(ctx: Context<Travel>, to_right: bool, password: String) -> Result<()> {
+        let player_account = &mut ctx.accounts.player_account;
+        let loot = **ctx.accounts.loot_account.to_account_info().try_borrow_mut_lamports()?;
+        
+        if to_right {
+            if player_account.position < 4 {
+                player_account.position += 1;
+            }
         } else {
-            game_data_account.player_position = game_data_account.player_position + 1;
-            print_player(game_data_account.player_position);
+            if player_account.position > 0 {
+                player_account.position -= 1;
+            }
         }
+
+        if player_account.position == 3 && loot > 0 {
+            if password == "gib" {
+                msg!("Loot received!");
+                **ctx.accounts.loot_account.to_account_info().try_borrow_mut_lamports()? -= loot;
+                **ctx.accounts.player.to_account_info().try_borrow_mut_lamports()? += loot;
+            }
+        }
+
+        print_game(player_account.position, loot > 0);
         Ok(())
     }
 }
 
-fn print_player(player_position: u8) {
-    if player_position == 0 {
-        msg!("A Journey Begins!");
-        msg!("o.......");
-    } else if player_position == 1 {
-        msg!("..o.....");
-    } else if player_position == 2 {
-        msg!("....o...");
-    } else if player_position == 3 {
-        msg!("........\\o/");
-        msg!("You have reached the end! Super!");
+fn print_game(player_position: u8, has_loot: bool) {
+    if has_loot {
+        match player_position {
+            0 => { msg!("o......💎."); }
+            1 => { msg!("..o....💎."); }
+            2 => { msg!("....o..💎."); }
+            3 => { msg!("......o💎."); }
+            4 => { msg!(".......💎o"); }
+            _ => ()
+        }
+    } else {
+        match player_position {
+            0 => { msg!("o........"); }
+            1 => { msg!("..o......"); }
+            2 => { msg!("....o...."); }
+            3 => { msg!("......o.."); }
+            4 => { msg!("........o"); }
+            _ => ()
+        }
     }
 }
 
 #[derive(Accounts)]
 pub struct Initialize<'info> {
-    #[account(
-        init_if_needed,
-        seeds = [b"level1"],
-        bump,
-        payer = signer,
-        space = 8 + 1
-    )]
-    pub new_game_data_account: Account<'info, GameDataAccount>,
     #[account(mut)]
     pub signer: Signer<'info>,
     pub system_program: Program<'info, System>,
+    #[account(init_if_needed, seeds = [b"player"], bump, payer = signer, space = 8 + 1)]
+    pub player_account: Account<'info, PlayerAccount>,
+    #[account(init_if_needed, seeds = [b"loot"], bump, payer = signer, space = 8)]
+    pub loot_account: Account<'info, LootAccount>,
 }
 
 #[derive(Accounts)]
-pub struct MoveLeft<'info> {
+pub struct NewGame<'info> {
     #[account(mut)]
-    pub game_data_account: Account<'info, GameDataAccount>,
+    pub payer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    #[account(mut)]
+    pub player_account: Account<'info, PlayerAccount>,
+    #[account(mut, seeds = [b"loot"], bump)]
+    pub loot_account: Account<'info, LootAccount>,
 }
 
 #[derive(Accounts)]
-pub struct MoveRight<'info> {
+pub struct Travel<'info> {
     #[account(mut)]
-    pub game_data_account: Account<'info, GameDataAccount>,
+    pub player: Signer<'info>,
+    #[account(mut)]
+    pub player_account: Account<'info, PlayerAccount>,
+    #[account(mut, seeds = [b"loot"], bump)]
+    pub loot_account: Account<'info, LootAccount>,
 }
 
 #[account]
-pub struct GameDataAccount {
-    player_position: u8,
+pub struct LootAccount {}
+
+#[account]
+pub struct PlayerAccount {
+    position: u8,
 }
